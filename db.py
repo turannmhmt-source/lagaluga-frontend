af28ba769d2b142df72f3abdf32f42636911fe57
turannmhmt-source <turann.mhmt@gmail.com>
2026-06-14 21:58:24 +0300

    Fix db.py remove supabase_admin

---
+++ db.py
@@ -0,0 +1,11 @@
+﻿import os
+from dotenv import load_dotenv
+from supabase import Client, create_client
+
+load_dotenv()
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL", "")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase_admin = None
